f626564b40e1f913e9458aaa2a6079b403818e0a
Christian Zacharias <christian.zacharias@gmail.com>
2012-11-07 16:18:18 +0100

    add german language folder

---
+++ modules/ac_module_internals/out/admin/de/ac_module_internalls_lang.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Module internals tools.
+ *
+ * @author Alfonsas Cirtautas
+ */
+
+$sLangName = "Deutsch";
+
+$aLang = array(
+    'charset' => 'ISO-8859-15',
+
+    'tbclmodule_internals_metadata' => 'Metadata',
+    'tbclmodule_internals_health'   => 'Status',
+    'tbclmodule_internals_utils'    => 'Utils',
+
+    'AC_MI_VERSION'    => 'Version',
+    'AC_MI_EXTEND'     => 'Extended Classes',
+    'AC_MI_FILES'      => 'Files',
+    'AC_MI_BLOCKS'     => 'Template Blocks',
+    'AC_MI_TEMPLATES'  => 'Templates',
+    'AC_MI_SETTINGS'   => 'Settings',
+    'AC_MI_EVENTS'     => 'Events',
+    'AC_MI_CACHE'      => 'Module cache',
+    'AC_MI_ACTIVATION' => 'Module Activation / Deactivation',
+
+    'AC_MI_FIXBTN'         => 'Fix',
+    'AC_MI_RESETBTN'       => 'Reset',
+    'AC_MI_ACTIVATEBTN'    => 'Activate',
+    'AC_MI_DEACTIVATEBTN'  => 'Deactivate',
+
+    'AC_LEGEND' => 'Legende',
+    'AC_STATE_OK'  => 'OK',
+    'AC_STATE_WA'  => 'Warning',
+    'AC_STATE_ER'  => 'Error',
+    'AC_STATE_FM'  => 'Fatal M',
+    'AC_STATE_FS'  => 'Fatal S',
+
+    'AC_STATE_OK_LABEL'  => 'Metadata matches data stored in shop',
+    'AC_STATE_WA_LABEL'  => 'Metadata missing in shop data',
+    'AC_STATE_ER_LABEL'  => 'Shop data not found in metadata',
+    'AC_STATE_FM_LABEL'  => 'Files defined in metadata does not exist',
+    'AC_STATE_FS_LABEL'  => 'Files defined in shop data does not exist'
+);
